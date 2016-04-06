b066cd09ba688da08e8493c38e341e5fa8506793
Chandler De Angelis <chandler.dea@me.com>
2016-04-05 19:06:41 -0700

    Added Carthage Support

---
+++ PaymentKit Example/SLPaymentKit/SLPaymentKit.h
@@ -0,0 +1,29 @@
+//
+//  SLPaymentKit.h
+//  SLPaymentKit
+//
+//  Created by Chandler De Angelis on 4/5/16.
+//  Copyright © 2016 Stripe. All rights reserved.
+//
+
+//! Project version number for SLPaymentKit.
+FOUNDATION_EXPORT double SLPaymentKitVersionNumber;
+
+//! Project version string for SLPaymentKit.
+FOUNDATION_EXPORT const unsigned char SLPaymentKitVersionString[];
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "PTKView.h"
+#import "PTKTextField.h"
+#import "PTKAddressZip.h"
+#import "PTKCardNumber.h"
+#import "PTKCardCVD.h"
+#import "PTKCardType.h"
+#import "PTKCardExpiry.h"
+#import "PTKCard.h"
+#import "PTKComponent.h"
+#import "PTKUSAddressZip.h"
+
+
