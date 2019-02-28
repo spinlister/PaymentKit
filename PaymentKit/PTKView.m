d14e262d77d2bc0aa88e3d72a706df825e1dd86a
Aiman Akhtar <aiman.akhtar@kiwitech.com>
2019-02-28 18:59:18 +0530

    When the next textfield was asked to become first responder, it was not working if it was being done in the same queue of the main thread, it had to be queued as another task in the same(main) queue. This issue was causing a crash because of the method stack overflow.

--- PaymentKit/PTKView.m
+++ PaymentKit/PTKView.m
@@ -426,8 +426,11 @@ static NSString *const kPTKOldLocalizedStringsTableName = @"STPaymentLocalizable
 
 - (void)pkTextFieldDidBackSpaceWhileTextIsEmpty:(PTKTextField *)textField
 {
-    if (textField == self.cardCVCField)
-        [self.cardExpiryField becomeFirstResponder];
+    if (textField == self.cardCVCField){
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self.cardExpiryField becomeFirstResponder];
+        });
+    }
     else if (textField == self.cardExpiryField)
         [self stateCardNumber];
 }
